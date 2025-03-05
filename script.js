04cccab76b08ebc5039f078cfa90561f91dd57bd
Deepak Rizal <deepak2000rizal@gmail.com>
2025-03-05 09:43:03 +0530

    feat: add media queries and share functionality for large screens

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const shareIcon = document.querySelector(".share-icon");
 const shareItems = document.querySelector(".share-items");
 const shareMobileIcon = document.querySelector(".share-mobile-icon");
+const desktopShareItems = document.querySelector(".desktop-share-items");
+
+const media = window.matchMedia("(min-width:700px)");
 
 shareIcon.addEventListener("click", () => {
-  shareItems.classList.remove("none");
+  if (media.matches) {
+    desktopShareItems.classList.toggle("none");
+  } else {
+    shareItems.classList.remove("none");
+  }
 });
 
 shareMobileIcon.addEventListener("click", () => {
